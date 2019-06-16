dbce7256feca7bbb91155f5627d7417e718312cd
Fabrizio Di Vittorio <fdivitto2013@gmail.com>
2019-06-16 10:11:20 +0200

    added combobox control to demo

--- examples/UI/TestControlsFrame.h
+++ examples/UI/TestControlsFrame.h
@@ -5,12 +5,13 @@ struct TestControlsFrame : public uiFrame {
   uiTextEdit * textEdit;
   uiButton * normalButton, * switchButton;
   uiListBox * listBox;
+  uiComboBox * comboBox1, * comboBox2;
 
   TestControlsFrame(uiFrame * parent)
-    : uiFrame(parent, "Test Controls", Point(150, 20), Size(320, 210), false) {
+    : uiFrame(parent, "Test Controls", Point(150, 20), Size(420, 240), false) {
 
-    new uiLabel(this, "Text Label:", Point(10,  30), Size(80, 20));
-    textEdit = new uiTextEdit(this, "Text Edit", Point(70,  30), Size(240, 20));
+    new uiLabel(this, "Text Label:", Point(10,  33));
+    textEdit = new uiTextEdit(this, "Text Edit", Point(70,  30), Size(340, 20));
     textEdit->anchors().right = true;
 
     normalButton = new uiButton(this, "Normal Button", Point(10, 60), Size(80, 20));
@@ -29,6 +30,33 @@ struct TestControlsFrame : public uiFrame {
     listBox->items().append("Listbox Row 5");
     listBox->items().append("Listbox Row 6");
     listBox->items().append("Listbox Row 7");
+
+    auto label = new uiLabel(this, "Normal ComboBox:", Point(170, 93));
+    label->anchors().left  = false;
+    label->anchors().right = true;
+    comboBox1 = new uiComboBox(this, Point(270, 90), Size(140, 20), 80);
+    comboBox1->anchors().left  = false;
+    comboBox1->anchors().right = true;
+    comboBox1->items().append("ComboBox Row 0");
+    comboBox1->items().append("ComboBox Row 1");
+    comboBox1->items().append("ComboBox Row 2");
+    comboBox1->items().append("ComboBox Row 3");
+    comboBox1->items().append("ComboBox Row 4");
+    comboBox1->items().append("ComboBox Row 5");
+    comboBox1->items().append("ComboBox Row 6");
+    comboBox1->items().append("ComboBox Row 7");
+    comboBox1->items().append("ComboBox Row 8");
+    comboBox1->items().append("ComboBox Row 9");
+    label = new uiLabel(this, "Editable ComboBox:", Point(170, 123));
+    label->anchors().left  = false;
+    label->anchors().right = true;
+    comboBox2 = new uiComboBox(this, Point(270, 120), Size(140, 20), 80);
+    comboBox2->comboBoxProps().openOnFocus = false;
+    comboBox2->textEditProps().hasCaret  = true;
+    comboBox2->textEditProps().allowEdit = true;
+    comboBox2->anchors().left  = false;
+    comboBox2->anchors().right = true;
+    comboBox2->items().copyFrom(comboBox1->items());
 
   }
 };
