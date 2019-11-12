ff1b0170a871253657c45c4d684e2dc519b1b6ca
Fabrizio Di Vittorio <fdivitto2013@gmail.com>
2019-11-12 16:05:24 +0100

    now can generate RGBA pixel format

--- tools/img2bitmap/img2bitmap.py
+++ tools/img2bitmap/img2bitmap.py
@@ -3,10 +3,13 @@
 # Converts an image (png, jpeg, ....) to FabGL Bitmap structure
 #
 # usage:
-#    img2bitmap filename [-t x y] [-s width height]
+#    img2bitmap filename [-t x y] [-s width height] [-d] [-f0 | -f1]
 #
-# -t = pixel where to take transparent color
-# -s = resize to specified values
+# -t  = pixel where to take transparent color
+# -s  = resize to specified values
+# -d  = enable dithering
+# -f0 = format is RGB2222 (default)
+# -f1 = format is RGB8888
 #
 # Example:
 #   python img2bitmap.py test.png -s 64 64 >out.c
@@ -21,14 +24,17 @@ from PIL import Image
 transpColorPos = None
 newSize = None
 dithering = False
+format = 0  # ABGR2222
 
 if len(sys.argv) < 2:
   print "Converts an image (png, jpeg, ....) to FabGL Bitmap structure"
   print "Usage:"
-  print "  python img2bitmap filename [-t x y] [-s width height] [-d]\n"
-  print "  -t = pixel where to take transparent color"
-  print "  -s = resize to specified values\n"
-  print "  -d = enable dithering\n"
+  print "  python img2bitmap filename [-t x y] [-s width height] [-d] [-f0 | -f1]\n"
+  print "  -t  = pixel where to take transparent color"
+  print "  -s  = resize to specified values\n"
+  print "  -d  = enable dithering\n"
+  print "  -f0 = format is ABGR2222 (default)\n"
+  print "  -f1 = format is RGBA8888\n"
   print "Example:"
   print "  python img2bitmap.py input.png -s 64 64 >out.c"
   sys.exit()
@@ -43,6 +49,12 @@ while i < len(sys.argv):
     i += 3
   elif sys.argv[i] == "-d":
     dithering = True
+    i += 1
+  elif sys.argv[i] == "-f0":
+    format = 0
+    i += 1
+  elif sys.argv[i] == "-f1":
+    format = 1
     i += 1
   else:
     i += 1
@@ -88,16 +100,30 @@ pix = im.load()
 
 print "const uint8_t {}_data[] = {{".format(name)
 
-for y in range(0, im.height):
-  print "\t",
-  for x in range(0, im.width):
-    v  = int(pix[x, y][0] / 255.0 * 3.0)        # R
-    v |= int(pix[x, y][1] / 255.0 * 3.0) << 2   # G
-    v |= int(pix[x, y][2] / 255.0 * 3.0) << 4   # B
-    v |= int(opix[x, y][3] / 255.0 * 3.0) << 6  # A
-    print "0x{:02x},".format(v),
-  print
+if format == 0:
+  formatstr = "PixelFormat::ABGR2222"
+  for y in range(0, im.height):
+    print "\t",
+    for x in range(0, im.width):
+      v  = int(pix[x, y][0] / 255.0 * 3.0)        # R
+      v |= int(pix[x, y][1] / 255.0 * 3.0) << 2   # G
+      v |= int(pix[x, y][2] / 255.0 * 3.0) << 4   # B
+      v |= int(opix[x, y][3] / 255.0 * 3.0) << 6  # A
+      print "0x{:02x},".format(v),
+    print
+
+if format == 1:
+  formatstr = "PixelFormat::RGBA8888"
+  for y in range(0, im.height):
+    print "\t",
+    for x in range(0, im.width):
+      print "0x{:02x},".format(pix[x, y][0]),
+      print "0x{:02x},".format(pix[x, y][1]),
+      print "0x{:02x},".format(pix[x, y][2]),
+      print "0x{:02x},".format(opix[x, y][3]),
+    print
+
 
 print "};"
 
-print "const Bitmap {} = Bitmap({}, {}, &{}_data[0]);".format(name, im.width, im.height, name)
+print "Bitmap {} = Bitmap({}, {}, &{}_data[0], {});".format(name, im.width, im.height, name, formatstr)
