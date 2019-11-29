7c33666b47a2d471901733faa65c5de0da1e9db0
Fabrizio Di Vittorio <fdivitto2013@gmail.com>
2019-11-29 23:06:48 +0100

    added I2C bus driver

---
+++ src/comdrivers/tsi2c.cpp
@@ -0,0 +1,199 @@
+/*
+  Created by Fabrizio Di Vittorio (fdivitto2013@gmail.com) - <http://www.fabgl.com>
+  Copyright (c) 2019 Fabrizio Di Vittorio.
+  All rights reserved.
+
+  This file is part of FabGL Library.
+
+  FabGL is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  FabGL is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with FabGL.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+#include "tsi2c.h"
+
+
+
+namespace fabgl {
+
+
+#define I2C_COMMTASK_STACK    1024
+#define I2C_COMMTASK_PRIORITY 5
+#define I2C_DEFAULT_FREQUENCY 100000
+
+
+#define EVTGROUP_READY (1 << 0)
+#define EVTGROUP_WRITE (1 << 1)
+#define EVTGROUP_READ  (1 << 2)
+#define EVTGROUP_DONE  (1 << 3)
+
+
+I2C::I2C(int bus)
+  : m_i2c(nullptr),
+    m_bus(bus),
+    m_commTaskHandle(nullptr),
+    m_eventGroup(nullptr)
+{
+}
+
+
+I2C::~I2C()
+{
+  end();
+}
+
+
+bool I2C::begin(gpio_num_t SDAGPIO, gpio_num_t SCLGPIO)
+{
+  m_SDAGPIO   = SDAGPIO;
+  m_SCLGPIO   = SCLGPIO;
+
+  m_eventGroup = xEventGroupCreate();
+
+  // why a task? Because esp32 i2c communications must be done on the same core
+  // must be pinned to one core (0 in this case)
+  xTaskCreatePinnedToCore(&commTaskFunc, "", I2C_COMMTASK_STACK, this, I2C_COMMTASK_PRIORITY, &m_commTaskHandle, 0);
+
+  // wait for commTaskFunc() ends initialization
+  xEventGroupWaitBits(m_eventGroup, EVTGROUP_DONE, true, false, portMAX_DELAY);
+
+  // ready to accept jobs
+  xEventGroupSetBits(m_eventGroup, EVTGROUP_READY);
+
+  return m_i2c != nullptr;
+}
+
+
+void I2C::end()
+{
+  if (m_commTaskHandle)
+    vTaskDelete(m_commTaskHandle);
+  m_commTaskHandle = nullptr;
+
+  if (m_i2c)
+    i2cRelease(m_i2c);
+  m_i2c = nullptr;
+
+  if (m_eventGroup)
+    vEventGroupDelete(m_eventGroup);
+  m_eventGroup = nullptr;
+}
+
+
+bool I2C::write(int address, uint8_t * buffer, int size, int frequency, int timeOutMS)
+{
+  // wait for I2C to be ready
+  xEventGroupWaitBits(m_eventGroup, EVTGROUP_READY, true, false, portMAX_DELAY);
+
+  m_jobInfo.frequency = frequency;
+  m_jobInfo.address   = address;
+  m_jobInfo.buffer    = buffer;
+  m_jobInfo.size      = size;
+  m_jobInfo.timeout   = timeOutMS;
+
+  /*
+  // unlock comm task for writing
+  xEventGroupSetBits(m_eventGroup, EVTGROUP_WRITE);
+
+  // wait for comm task to finish job
+  xEventGroupWaitBits(m_eventGroup, EVTGROUP_DONE, true, false, portMAX_DELAY);
+  */
+  xEventGroupSync(m_eventGroup, EVTGROUP_WRITE, EVTGROUP_DONE, portMAX_DELAY);
+
+
+  bool ret = (m_jobInfo.lastError == I2C_ERROR_OK);
+
+  // makes I2C ready for new requests
+  xEventGroupSetBits(m_eventGroup, EVTGROUP_READY);
+
+  return ret;
+}
+
+
+int I2C::read(int address, uint8_t * buffer, int size, int frequency, int timeOutMS)
+{
+  // wait for I2C to be ready
+  xEventGroupWaitBits(m_eventGroup, EVTGROUP_READY, true, false, portMAX_DELAY);
+
+  m_jobInfo.frequency = frequency;
+  m_jobInfo.address   = address;
+  m_jobInfo.buffer    = buffer;
+  m_jobInfo.size      = size;
+  m_jobInfo.timeout   = timeOutMS;
+
+  /*
+  // unlock comm task for reading
+  xEventGroupSetBits(m_eventGroup, EVTGROUP_READ);
+
+  // wait for comm task to finish job
+  xEventGroupWaitBits(m_eventGroup, EVTGROUP_DONE, true, false, portMAX_DELAY);
+  */
+  xEventGroupSync(m_eventGroup, EVTGROUP_READ, EVTGROUP_DONE, portMAX_DELAY);
+
+  int ret = m_jobInfo.readCount;
+
+  // makes I2C ready for new requests
+  xEventGroupSetBits(m_eventGroup, EVTGROUP_READY);
+
+  return ret;
+}
+
+
+
+void I2C::commTaskFunc(void * pvParameters)
+{
+  I2C * ths = (I2C*) pvParameters;
+
+  i2c_t * i2c = i2cInit(ths->m_bus, ths->m_SDAGPIO, ths->m_SCLGPIO, I2C_DEFAULT_FREQUENCY);
+  if (!i2c)
+    return;
+
+  i2cFlush(i2c);
+
+  ths->m_i2c = i2c;
+
+  // get initial default frequency
+  int freq = i2cGetFrequency(i2c);
+
+  I2CJobInfo * job = &ths->m_jobInfo;
+
+  // main send/receive loop
+  while (true) {
+
+    // unlock waiting task
+    xEventGroupSetBits(ths->m_eventGroup, EVTGROUP_DONE);
+
+    // wait for another job
+    auto bits = xEventGroupWaitBits(ths->m_eventGroup, EVTGROUP_WRITE | EVTGROUP_READ, true, false, portMAX_DELAY);
+
+    // setup frequency if necessary
+    if (freq != job->frequency) {
+      freq = job->frequency;
+      i2cSetFrequency(i2c, freq);
+    }
+
+    if (bits & EVTGROUP_WRITE)
+      job->lastError = i2cWrite(i2c, job->address, job->buffer, job->size, true, job->timeout);
+    else if (bits & EVTGROUP_READ)
+      job->lastError = i2cRead(i2c, job->address, job->buffer, job->size, true, job->timeout, &job->readCount);
+
+  }
+
+}
+
+
+
+} // end of namespace
---
+++ src/comdrivers/tsi2c.h
@@ -0,0 +1,162 @@
+/*
+  Created by Fabrizio Di Vittorio (fdivitto2013@gmail.com) - <http://www.fabgl.com>
+  Copyright (c) 2019 Fabrizio Di Vittorio.
+  All rights reserved.
+
+  This file is part of FabGL Library.
+
+  FabGL is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  FabGL is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with FabGL.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+#pragma once
+
+
+#include <stdint.h>
+#include <stddef.h>
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/queue.h"
+#include "freertos/semphr.h"
+
+#include "esp32-hal.h"
+
+#include "fabglconf.h"
+
+
+
+/**
+ * @file
+ *
+ * @brief This file contains fabgl::I2C definition.
+ */
+
+
+namespace fabgl {
+
+
+struct I2CJobInfo {
+  int32_t   frequency;
+  uint8_t * buffer;
+  uint8_t   address;
+  uint16_t  size;
+  uint16_t  timeout;
+  uint32_t  readCount;
+  i2c_err_t lastError;
+};
+
+
+/**
+ * @brief I2C class allows multiple tasks to communicate with I2C devices, serializing read/write jobs.
+ *
+ * A single instance of I2C class can be shared among multiple tasks or timers (not interrupts).
+ *
+ * Example:
+ *
+ *     fabgl::I2C I2C;
+ *
+ *     void setup() {
+ *       I2C.begin(GPIO_NUM_4, GPIO_NUM_15);  // 4 = SDA, 15 = SCL
+ *     }
+ */
+class I2C {
+
+public:
+
+  /**
+   * @brief I2C class constructor
+   *
+   * @param bus I2C bus to use. Allowed 0 or 1 (default is 0).
+   */
+  I2C(int bus = 0);
+
+  ~I2C();
+
+  /**
+   * @brief Initializes I2C instance associating GPIOs to I2C signals.
+   *
+   * @param SDAGPIO Pin to use for SDA signal
+   * @param SCLGPIO Pin to use for SCK signal
+   *
+   * Example:
+   *
+   *     // 4 = SDA, 15 = SCL
+   *     I2C.begin(GPIO_NUM_4, GPIO_NUM_15);
+   */
+  bool begin(gpio_num_t SDAGPIO, gpio_num_t SCLGPIO);
+
+  void end();
+
+  /**
+   * @brief Sends a buffer to I2C bus.
+   *
+   * This is a thread-safe operation. Multiple tasks can call this method concurrently.
+   *
+   * @param address I2C address of the destination slave device
+   * @param buffer Buffer to send
+   * @param size Number of bytes to send
+   * @param frequency Clock frequency
+   * @param timeOutMS Operation timeout in milliseconds
+   *
+   * @return True on success
+   */
+  bool write(int address, uint8_t * buffer, int size, int frequency = 100000, int timeOutMS = 50);
+
+  /**
+   * @brief Receives a buffer from I2C bus
+   *
+   * This is a thread-safe operation. Multiple tasks can call this methods concurrently.
+   *
+   * @param address I2C address of the source slave device
+   * @param buffer Buffer where to put received data (must have space for at least "size" bytes)
+   * @param size Number of bytes to receive
+   * @param frequency Clock frequency
+   * @param timeOutMS Operation timeout in milliseconds
+   *
+   * @return Number of actually read bytes
+   */
+  int read(int address, uint8_t * buffer, int size, int frequency = 100000, int timeOutMS = 50);
+
+
+private:
+
+  static void commTaskFunc(void * pvParameters);
+
+
+  i2c_t *            m_i2c;
+
+  uint8_t            m_bus;
+  gpio_num_t         m_SDAGPIO;
+  gpio_num_t         m_SCLGPIO;
+
+  TaskHandle_t       m_commTaskHandle;
+
+  EventGroupHandle_t m_eventGroup;
+
+  I2CJobInfo         m_jobInfo;
+};
+
+
+
+
+
+
+
+
+
+
+} // end of namespace
+
+
+
