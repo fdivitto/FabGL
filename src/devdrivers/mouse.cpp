b36f204a556e85d4a28087f4788f2b207ed48c20
Fabrizio Di Vittorio <fdivitto2013@gmail.com>
2020-02-07 13:25:25 +0100

    doc fix

--- src/devdrivers/mouse.cpp
+++ src/devdrivers/mouse.cpp
@@ -125,7 +125,7 @@ bool Mouse::getNextDelta(MouseDelta * delta, int timeOutMS, bool requestResendOn
       return false;  // timeout
   }
 
-  // the unique way we have to check packed is disaligned, the bit 4 of first byte must be always 1
+  // the unique way we have to check a packet is disaligned: the bit 4 of first byte must be always 1
   if ((rcv[0] & 8) == 0) {
     PS2Controller::instance()->warmInit();
     return false;
