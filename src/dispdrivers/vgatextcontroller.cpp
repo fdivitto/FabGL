81d5960747ec0214acb21bd995ba5b8f4370bcd8
Fabrizio Di Vittorio <fdivitto2013@gmail.com>
2020-07-15 23:45:23 +0200

    pinned rendering to core 1

--- src/dispdrivers/vgatextcontroller.cpp
+++ src/dispdrivers/vgatextcontroller.cpp
@@ -252,7 +252,7 @@ void VGATextController::setResolution(VGATimings const& timings)
 
 
   // ESP_INTR_FLAG_LEVEL1: should be less than PS2Controller interrupt level
-  esp_intr_alloc(ETS_I2S1_INTR_SOURCE, ESP_INTR_FLAG_LEVEL1 | ESP_INTR_FLAG_IRAM, I2SInterrupt, this, &m_isr_handle);
+  esp_intr_alloc_pinnedToCore(ETS_I2S1_INTR_SOURCE, ESP_INTR_FLAG_LEVEL1 | ESP_INTR_FLAG_IRAM, I2SInterrupt, this, &m_isr_handle, 1);
 
   m_GPIOStream.play(m_timings.frequency, m_DMABuffers);
 
