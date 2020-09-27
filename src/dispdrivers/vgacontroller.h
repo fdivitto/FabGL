72a0cd8badd90b4d46395d37bfd26b42bd3a2142
Fabrizio Di Vittorio <fdivitto2013@gmail.com>
2020-09-27 23:33:07 +0200

    now VGAController is based on VGABaseController

--- src/dispdrivers/vgacontroller.h
+++ src/dispdrivers/vgacontroller.h
@@ -45,64 +45,13 @@
 #include "fabutils.h"
 #include "devdrivers/swgenerator.h"
 #include "displaycontroller.h"
+#include "dispdrivers/vgabasecontroller.h"
 
 
 
 
 namespace fabgl {
 
-
-#define VGA_RED_BIT   0
-#define VGA_GREEN_BIT 2
-#define VGA_BLUE_BIT  4
-#define VGA_HSYNC_BIT 6
-#define VGA_VSYNC_BIT 7
-
-#define VGA_SYNC_MASK ((1 << VGA_HSYNC_BIT) | (1 << VGA_VSYNC_BIT))
-
-
-// pixel 0 = byte 2, pixel 1 = byte 3, pixel 2 = byte 0, pixel 3 = byte 1 :
-// pixel : 0  1  2  3  4  5  6  7  8  9 10 11 ...etc...
-// byte  : 2  3  0  1  6  7  4  5 10 11  8  9 ...etc...
-// dword : 0           1           2          ...etc...
-// Thanks to https://github.com/paulscottrobson for the new macro. Before was: (row[((X) & 0xFFFC) + ((2 + (X)) & 3)])
-#define VGA_PIXELINROW(row, X) (row[(X) ^ 2])
-
-// requires variables: m_viewPort
-#define VGA_PIXEL(X, Y)    VGA_PIXELINROW(m_viewPort[(Y)], X)
-#define VGA_INVERT_PIXEL(X, Y) { auto px = &VGA_PIXEL((X), (Y)); *px = ~(*px ^ VGA_SYNC_MASK); }
-
-
-
-/** \ingroup Enumerations
- * @brief Represents one of the four blocks of horizontal or vertical line
- */
-enum VGAScanStart {
-  FrontPorch,   /**< Horizontal line sequence is: FRONTPORCH -> SYNC -> BACKPORCH -> VISIBLEAREA */
-  Sync,         /**< Horizontal line sequence is: SYNC -> BACKPORCH -> VISIBLEAREA -> FRONTPORCH */
-  BackPorch,    /**< Horizontal line sequence is: BACKPORCH -> VISIBLEAREA -> FRONTPORCH -> SYNC */
-  VisibleArea   /**< Horizontal line sequence is: VISIBLEAREA -> FRONTPORCH -> SYNC -> BACKPORCH */
-};
-
-
-/** @brief Specifies the VGA timings. This is a modeline decoded. */
-struct VGATimings {
-  char          label[22];       /**< Resolution text description */
-  int           frequency;       /**< Pixel frequency (in Hz) */
-  int16_t       HVisibleArea;    /**< Horizontal visible area length in pixels */
-  int16_t       HFrontPorch;     /**< Horizontal Front Porch duration in pixels */
-  int16_t       HSyncPulse;      /**< Horizontal Sync Pulse duration in pixels */
-  int16_t       HBackPorch;      /**< Horizontal Back Porch duration in pixels */
-  int16_t       VVisibleArea;    /**< Vertical number of visible lines */
-  int16_t       VFrontPorch;     /**< Vertical Front Porch duration in lines */
-  int16_t       VSyncPulse;      /**< Vertical Sync Pulse duration in lines */
-  int16_t       VBackPorch;      /**< Vertical Back Porch duration in lines */
-  char          HSyncLogic;      /**< Horizontal Sync polarity '+' or '-' */
-  char          VSyncLogic;      /**< Vertical Sync polarity '+' or '-' */
-  uint8_t       scanCount;       /**< Scan count. 1 = single scan, 2 = double scan (allowing low resolutions like 320x240...) */
-  uint8_t       multiScanBlack;  /**< 0 = Additional rows are the repetition of the first. 1 = Additional rows are blank. */
-  VGAScanStart  HStartingBlock;  /**< Horizontal starting block. DetermineshHorizontal order of signals */
-};
 
 
 
@@ -134,7 +83,7 @@ struct VGATimings {
 *     // Set 640x350@70Hz resolution
 *     VGAController.setResolution(VGA_640x350_70Hz);
 */
-class VGAController : public GenericBitmappedDisplayController {
+class VGAController : public VGABaseController {
 
 public:
 
@@ -152,167 +101,18 @@ public:
    */
   static VGAController * instance() { return s_instance; }
 
-
-  /**
-   * @brief This is the 8 colors (5 GPIOs) initializer.
-   *
-   * One GPIO per channel, plus horizontal and vertical sync signals.
-   *
-   * @param redGPIO GPIO to use for red channel.
-   * @param greenGPIO GPIO to use for green channel.
-   * @param blueGPIO GPIO to use for blue channel.
-   * @param HSyncGPIO GPIO to use for horizontal sync signal.
-   * @param VSyncGPIO GPIO to use for vertical sync signal.
-   *
-   * Example:
-   *
-   *     // Use GPIO 22 for red, GPIO 21 for green, GPIO 19 for blue, GPIO 18 for HSync and GPIO 5 for VSync
-   *     VGAController.begin(GPIO_NUM_22, GPIO_NUM_21, GPIO_NUM_19, GPIO_NUM_18, GPIO_NUM_5);
-   */
-  void begin(gpio_num_t redGPIO, gpio_num_t greenGPIO, gpio_num_t blueGPIO, gpio_num_t HSyncGPIO, gpio_num_t VSyncGPIO);
-
-  /**
-   * @brief This is the 64 colors (8 GPIOs) initializer.
-   *
-   * Two GPIOs per channel, plus horizontal and vertical sync signals.
-   *
-   * @param red1GPIO GPIO to use for red channel, MSB bit.
-   * @param red0GPIO GPIO to use for red channel, LSB bit.
-   * @param green1GPIO GPIO to use for green channel, MSB bit.
-   * @param green0GPIO GPIO to use for green channel, LSB bit.
-   * @param blue1GPIO GPIO to use for blue channel, MSB bit.
-   * @param blue0GPIO GPIO to use for blue channel, LSB bit.
-   * @param HSyncGPIO GPIO to use for horizontal sync signal.
-   * @param VSyncGPIO GPIO to use for vertical sync signal.
-   *
-   * Example:
-   *
-   *     // Use GPIO 22-21 for red, GPIO 19-18 for green, GPIO 5-4 for blue, GPIO 23 for HSync and GPIO 15 for VSync
-   *     VGAController.begin(GPIO_NUM_22, GPIO_NUM_21, GPIO_NUM_19, GPIO_NUM_18, GPIO_NUM_5, GPIO_NUM_4, GPIO_NUM_23, GPIO_NUM_15);
-   */
-  void begin(gpio_num_t red1GPIO, gpio_num_t red0GPIO, gpio_num_t green1GPIO, gpio_num_t green0GPIO, gpio_num_t blue1GPIO, gpio_num_t blue0GPIO, gpio_num_t HSyncGPIO, gpio_num_t VSyncGPIO);
-
-  /**
-   * @brief This is the 64 colors (8 GPIOs) initializer using default pinout.
-   *
-   * Two GPIOs per channel, plus horizontal and vertical sync signals.
-   * Use GPIO 22-21 for red, GPIO 19-18 for green, GPIO 5-4 for blue, GPIO 23 for HSync and GPIO 15 for VSync
-   *
-   * Example:
-   *
-   *     VGAController.begin();
-   */
-  void begin();
-
-  void end();
-
   // abstract method of BitmappedDisplayController
   void suspendBackgroundPrimitiveExecution();
 
   // abstract method of BitmappedDisplayController
   void resumeBackgroundPrimitiveExecution();
 
-  /**
-   * @brief Gets number of bits allocated for each channel.
-   *
-   * Number of bits depends by which begin() initializer has been called.
-   *
-   * @return 1 (8 colors) or 2 (64 colors).
-   */
-  uint8_t getBitsPerChannel() { return m_bitsPerChannel; }
-
   // abstract method of BitmappedDisplayController
   NativePixelFormat nativePixelFormat() { return NativePixelFormat::SBGR2222; }
 
-  /**
-   * @brief Sets current resolution using linux-like modeline.
-   *
-   * Modeline must have following syntax (non case sensitive):
-   *
-   *     "label" clock_mhz hdisp hsyncstart hsyncend htotal vdisp vsyncstart vsyncend vtotal (+HSync | -HSync) (+VSync | -VSync) [DoubleScan | QuadScan] [FrontPorchBegins | SyncBegins | BackPorchBegins | VisibleBegins] [MultiScanBlank]
-   *
-   * In fabglconf.h there are macros with some predefined modelines for common resolutions.
-   * When MultiScanBlank and DoubleScan is specified then additional rows are not repeated, but just filled with blank lines.
-   *
-   * @param modeline Linux-like modeline as specified above.
-   * @param viewPortWidth Horizontal viewport size in pixels. If less than zero (-1) it is sized to modeline visible area width.
-   * @param viewPortHeight Vertical viewport size in pixels. If less then zero (-1) it is sized to maximum allocable.
-   * @param doubleBuffered if True allocates another viewport of the same size to use as back buffer. Make sure there is enough free memory.
-   *
-   * Example:
-   *
-   *     // Use predefined modeline for 640x480@60Hz
-   *     VGAController.setResolution(VGA_640x480_60Hz);
-   *
-   *     // The same of above using modeline string
-   *     VGAController.setResolution("\"640x480@60Hz\" 25.175 640 656 752 800 480 490 492 525 -HSync -VSync");
-   *
-   *     // Set 640x382@60Hz but limit the viewport to 640x350
-   *     VGAController.setResolution(VGA_640x382_60Hz, 640, 350);
-   *
-   */
-  void setResolution(char const * modeline, int viewPortWidth = -1, int viewPortHeight = -1, bool doubleBuffered = false);
+  using VGABaseController::setResolution;
 
   void setResolution(VGATimings const& timings, int viewPortWidth = -1, int viewPortHeight = -1, bool doubleBuffered = false);
-
-  VGATimings * getResolutionTimings() { return &m_timings; }
-
-  // abstract method of BitmappedDisplayController
-  int getScreenWidth()    { return m_timings.HVisibleArea; }
-
-  // abstract method of BitmappedDisplayController
-  int getScreenHeight()   { return m_timings.VVisibleArea; }
-
-  /**
-   * @brief Determines horizontal position of the viewport.
-   *
-   * @return Horizontal position of the viewport (in case viewport width is less than screen width).
-   */
-  int getViewPortCol()    { return m_viewPortCol; }
-
-  /**
-   * @brief Determines vertical position of the viewport.
-   *
-   * @return Vertical position of the viewport (in case viewport height is less than screen height).
-   */
-  int getViewPortRow()    { return m_viewPortRow; }
-
-  // abstract method of BitmappedDisplayController
-  int getViewPortWidth()  { return m_viewPortWidth; }
-
-  // abstract method of BitmappedDisplayController
-  int getViewPortHeight() { return m_viewPortHeight; }
-
-  /**
-   * @brief Moves screen by specified horizontal and vertical offset.
-   *
-   * Screen moving is performed moving horizontal and vertical Front and Back porchs.
-   *
-   * @param offsetX Horizontal offset in pixels. < 0 goes left, > 0 goes right.
-   * @param offsetY Vertical offset in pixels. < 0 goes up, > 0 goes down.
-   *
-   * Example:
-   *
-   *     // Move screen 4 pixels right, 1 pixel left
-   *     VGAController.moveScreen(4, -1);
-   */
-  void moveScreen(int offsetX, int offsetY);
-
-  /**
-   * @brief Reduces or expands screen size by the specified horizontal and vertical offset.
-   *
-   * Screen shrinking is performed changing horizontal and vertical Front and Back porchs.
-   *
-   * @param shrinkX Horizontal offset in pixels. > 0 shrinks, < 0 expands.
-   * @param shrinkY Vertical offset in pixels. > 0 shrinks, < 0 expands.
-   *
-   * Example:
-   *
-   *     // Shrink screen by 8 pixels and move by 8 pixels to the left
-   *     VGAController.shrinkScreen(8, 0);
-   *     VGAController.moveScreen(8, 0);
-   */
-  void shrinkScreen(int shrinkX, int shrinkY);
 
   /**
    * @brief Reads pixels inside the specified rectangle.
@@ -402,38 +202,13 @@ public:
    */
   void setRawPixel(int x, int y, uint8_t rgb) { VGA_PIXEL(x, y) = rgb; }
 
-  /**
-   * @brief Gets a raw scanline pointer.
-   *
-   * A raw scanline must be filled with raw pixel colors. Use VGAController.createRawPixel to create raw pixel colors.
-   * A raw pixel (or raw color) is a byte (uint8_t) that contains color information and synchronization signals.
-   * Pixels are arranged in 32 bit packes as follows:
-   *   pixel 0 = byte 2, pixel 1 = byte 3, pixel 2 = byte 0, pixel 3 = byte 1 :
-   *   pixel : 0  1  2  3  4  5  6  7  8  9 10 11 ...etc...
-   *   byte  : 2  3  0  1  6  7  4  5 10 11  8  9 ...etc...
-   *   dword : 0           1           2          ...etc...
-   *
-   * @param y Vertical scanline position (0 = top row)
-   */
-  uint8_t * getScanline(int y)                { return (uint8_t*) m_viewPort[y]; }
-
-  static bool convertModelineToTimings(char const * modeline, VGATimings * timings);
-
 private:
 
   void init();
 
-  uint8_t packHVSync(bool HSync, bool VSync);
-  uint8_t preparePixel(RGB222 rgb) { return m_HVSync | (rgb.B << VGA_BLUE_BIT) | (rgb.G << VGA_GREEN_BIT) | (rgb.R << VGA_RED_BIT); }
-  uint8_t preparePixelWithSync(RGB222 rgb, bool HSync, bool VSync);
-
-  void freeBuffers();
-  void fillHorizBuffers(int offsetX);
-  void fillVertBuffers(int offsetY);
-  int fill(uint8_t volatile * buffer, int startPos, int length, uint8_t red, uint8_t green, uint8_t blue, bool hsync, bool vsync);
   void allocateViewPort();
-  void freeViewPort();
-  int calcRequiredDMABuffersCount(int viewPortHeight);  
+
+  void onSetupDMABuffer(lldesc_t volatile * buffer, bool isStartOfVertFrontPorch, int scan, bool isVisible, int visibleRow);
 
   // abstract method of BitmappedDisplayController
   void setPixelAt(PixelDesc const & pixelDesc, Rect & updateRect);
@@ -494,57 +269,9 @@ private:
 
   static void VSyncInterrupt(void * arg);
 
-  static void setupGPIO(gpio_num_t gpio, int bit, gpio_mode_t mode);
-
-  // DMA related methods
-  bool setDMABuffersCount(int buffersCount);
-  void setDMABufferBlank(int index, void volatile * address, int length);
-  void setDMABufferView(int index, int row, int scan, volatile uint8_t * * viewPort, bool onVisibleDMA);
-  void setDMABufferView(int index, int row, int scan);
-  void volatile * getDMABuffer(int index, int * length);
-
-
   static VGAController * s_instance;
 
-  intr_handle_t          m_isr_handle;
-
-  volatile int           m_VSyncInterruptSuspended;             // 0 = enabled, >0 suspended
-
-  GPIOStream             m_GPIOStream;
-
-  int                    m_bitsPerChannel;  // 1 = 8 colors, 2 = 64 colors, set by begin()
-  VGATimings             m_timings;
-  int16_t                m_linesCount;
   volatile int16_t       m_maxVSyncISRTime; // Maximum us VSync interrupt routine can run
-
-  // These buffers contains a full line, with FrontPorch, Sync, BackPorch and blank visible area, in the
-  // order specified by timings.HStartingBlock
-  volatile uint8_t *     m_HBlankLine_withVSync;
-  volatile uint8_t *     m_HBlankLine;
-  int16_t                m_HLineSize;
-
-  // contains H and V signals for visible line
-  volatile uint8_t       m_HVSync;
-
-  volatile int16_t       m_viewPortCol;
-  volatile int16_t       m_viewPortRow;
-  volatile int16_t       m_viewPortWidth;
-  volatile int16_t       m_viewPortHeight;
-
-  // when double buffer is enabled the "drawing" view port is always m_viewPort, while the "visible" view port is always m_viewPortVisible
-  // when double buffer is not enabled then m_viewPort = m_viewPortVisible
-  volatile uint8_t * *   m_viewPort;
-  volatile uint8_t * *   m_viewPortVisible;
-
-  uint8_t *              m_viewPortMemoryPool[FABGLIB_VIEWPORT_MEMORY_POOL_COUNT + 1];  // last allocated pool is nullptr
-
-  // when double buffer is enabled the running DMA buffer is always m_DMABuffersRunning
-  // when double buffer is not enabled then m_DMABuffers = m_DMABuffersRunning
-  lldesc_t volatile *    m_DMABuffersHead;
-  lldesc_t volatile *    m_DMABuffers;
-  lldesc_t volatile *    m_DMABuffersVisible;
-
-  int                    m_DMABuffersCount;
 
 };
 
