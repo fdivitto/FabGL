b57aaaa246b3c311c3dc90993cb5b4d6f1629f38
Fabrizio Di Vittorio <fdivitto2013@gmail.com>
2020-10-01 22:24:49 +0200

    added to include fabgl as a component of a native ESP-IDF project.

---
+++ component.mk
@@ -0,0 +1,5 @@
+
+COMPONENT_ADD_INCLUDEDIRS=FabGL/src FabGL/src/devdrivers FabGL/src/comdrivers FabGL/src/dispdrivers FabGL/src/fonts FabGL/src/images
+COMPONENT_SRCDIRS=FabGL/src FabGL/src/devdrivers FabGL/src/comdrivers FabGL/src/dispdrivers FabGL/src/fonts FabGL/src/images
+CXXFLAGS += -O2
+
