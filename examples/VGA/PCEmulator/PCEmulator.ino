85c2676c99bb95a06c8ed04d1129dc3a14b83e57
Fabrizio Di Vittorio <fdivitto2013@gmail.com>
2021-02-09 00:34:50 +0100

    added link to fd.img file

--- examples/VGA/PCEmulator/PCEmulator.ino
+++ examples/VGA/PCEmulator/PCEmulator.ino
@@ -24,7 +24,7 @@
 
     - to run this application you need an ESP32 with PSRAM installed and an SD-CARD slot (ie TTGO ESP32 v1.4)
     - open this with Arduino and make sure PSRAM is DISABLED
-    - copy the boot floppy disk image (fd.img) into the SD-CARD ()
+    - copy the boot floppy disk image (fd.img) into the SD-CARD (https://github.com/fdivitto/FabGL/blob/master/examples/VGA/PCEmulator/fd.img)
     - compile and upload the sketch
 
  */
